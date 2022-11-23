21d12f4cad1f360441644b144b035048d400cdc2
Vita <spivakovav@mail.ru>
2022-11-23 22:46:58 +0300

    task 2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+
+Console.Write("Максимум равен ");
+Console.WriteLine(max);
