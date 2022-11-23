b0c9c0ea1f37522c73530177c7e16cde373a50c9
Vita <spivakovav@mail.ru>
2022-11-24 00:01:40 +0300

    Task 8

---
+++ task8/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int number = 1;
+int temp = 0;
+
+if (n > 0)
+
+  while (number < n)
+  {
+      if (number % 2 == 0)  
+       Console.WriteLine(number);
+  number ++;
+  }
+
+if (n < 0)   
+  
+  while (number < -n)
+      {
+      if (number % 2 == 0) Console.WriteLine(temp = number * -1);
+      number ++;
+    }
