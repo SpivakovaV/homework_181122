4b7e53b35501589996636ce6f400565746fd6d1b
Vita <spivakovav@mail.ru>
2022-11-23 23:14:09 +0300

    Task 6

---
+++ task6/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 0) a = a * -1;
+
+if (a % 2==1)
+            {
+                Console.WriteLine("Число нечетное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
